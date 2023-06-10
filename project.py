ab3a2a548373c7725c97a263fde41cd34690ba74
Dongeun Lee <aoiu981@gmail.com>
2023-06-10 21:57:02 +0900

    crawling2

--- project.py
+++ project.py
@@ -15,19 +15,24 @@ driver.get(URL)
 # 노래 정보 dataframe
 df = pd.DataFrame({'song_name':[], 'artist_name':[], 'release_date':[], 'song_genre':[]})
 
+
 # 위에서 50개
-for i in range(50):
-    driver.find_element(By.XPATH, '//*[@id="frm"]/div/table/tbody/tr[{0}]/td[4]/div/a'.format(i+1)).click()
-    driver.implicitly_wait(2)
+for page in range(1, 10):
+    for i in range(50):
+        driver.find_element(By.XPATH, '//*[@id="frm"]/div/table/tbody/tr[{0}]/td[4]/div/a'.format(i+1)).click()
+        driver.implicitly_wait(2)
 
-    song_name = driver.find_element(By.CLASS_NAME, 'song_name').text
-    artist_name = driver.find_element(By.XPATH, '//*[@id="downloadfrm"]/div/div/div[2]/div[1]/div[2]/a/span[1]').text
-    release_date = driver.find_element(By.XPATH, '//*[@id="downloadfrm"]/div/div/div[2]/div[2]/dl/dd[2]').text
-    song_genre = driver.find_element(By.XPATH, '//*[@id="downloadfrm"]/div/div/div[2]/div[2]/dl/dd[3]').text
-
-    df.loc[i] = [song_name, artist_name, release_date, song_genre]
-
+        song_name = driver.find_element(By.CLASS_NAME, 'song_name').text
+        artist_name = driver.find_element(By.XPATH, '//*[@id="downloadfrm"]/div/div/div[2]/div[1]/div[2]/a/span[1]').text
+        release_date = driver.find_element(By.XPATH, '//*[@id="downloadfrm"]/div/div/div[2]/div[2]/dl/dd[2]').text
+        song_genre = driver.find_element(By.XPATH, '//*[@id="downloadfrm"]/div/div/div[2]/div[2]/dl/dd[3]').text
+    
+    
     driver.back()
+    driver.find_element(By.XPATH, '//*[@id="pageObjNavgation"]/div/span/a[{0}]'.format(page)).click()
+
+
+df.loc[i] = [song_name, artist_name, release_date, song_genre]
 
 
 print(df)
